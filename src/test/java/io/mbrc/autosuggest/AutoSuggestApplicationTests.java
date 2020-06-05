524978ce2906e8539f02a7c2a96ac10d52ff7ad3
mbrc12 <mbrc12@gmail.com>
2020-06-06 00:45:52 +0530

    Added some files to git.

---
+++ src/test/java/io/mbrc/autosuggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mbrc.autosuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
